9a8bab22483ae473a6c2400ae3e9cb900cceaf22
Ivaylo Tsochev <ivo.tsochev@live.com>
2021-09-23 11:36:56 +0300

    styling updates on Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,20 +7,24 @@ function Home() {
     return (
         <div className='home'>
             <div className="home__container">
+
                 <img className='home__image' src="https://m.media-amazon.com/images/I/71yDly3tmEL._SX3000_.jpg" alt="" />
-            </div>
-            <div className="home__row">
-                <Product />
-            </div>
 
-            <div className="home__row">
-                {/* Product */}
-                {/* Product */}
-                {/* Product */}
-            </div>
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                </div>
 
-            <div className="home__row">
-                {/* Product */}
+                <div className="home__row">
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+
+                <div className="home__row">
+                    <Product />
+                </div>
+
             </div>
         </div>
     )
